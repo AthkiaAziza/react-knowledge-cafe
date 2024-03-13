9377297251157e570d17871186d2c9763154fe37
AthkiaAziza <athkiaaziza@gmail.com>
2024-03-13 11:32:47 +0600

    added bookmark and reading time

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,20 +1,22 @@
 import PropTypes from 'prop-types';
+import { CiBookmark } from "react-icons/ci";
 
-const Blog = ({ blog }) => {
-    const { title, cover, author, author_Img, reading_time, posted_date, hashtags } = blog;
+const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
+    const { title, cover, author, author_Img, time, posted_date, hashtags } = blog;
     return (
-        <div>
+        <div className='space-y-5 mb-20'>
             <img src={cover} alt={`Cover picture of the title ${title}`} />
             <div className='flex justify-between'>
                 <div className='flex'>
-                    <img className='w-16 h-16 rounded-full' src={author_Img} alt="" />
+                    <img className='w-16 mb-8 h-16 rounded-full' src={author_Img} alt="" />
                     <div className='mx-3'>
                         <h3 className='text-2xl'>{author}</h3>
                         <p>{posted_date}</p>
                     </div>
                 </div>
                 <div>
-                    <span>{reading_time} min read</span>
+                    <span className='text-2xl'>{time} min read</span>
+                    <button onClick={() => handleAddToBookmark(blog)} className='ml-2 text-2xl'><CiBookmark></CiBookmark></button>
                 </div>
             </div>
             <h2 className='text-4xl'>{title}</h2>
@@ -23,12 +25,18 @@ const Blog = ({ blog }) => {
                     hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
                 }
             </p>
+            <button
+            onClick={ () => handleMarkAsRead(time)}
+             className='text-purple-700 font-bold underline'
+             >Mark As Read</button>
         </div>
     );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func.isRequired,
+    handleMarkAsRead: PropTypes.func.isRequired
 }
 
 export default Blog;
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,11 +1,25 @@
+import PropTypes from 'prop-types';
+import Bookmark from '../Bookmark/Bookmark';
 
-
-const Bookmarks = () => {
+const Bookmarks = ({bookmarks, readingTime}) => {
     return (
-        <div className="md:w-1/3">
-          <h2>Bookmarks</h2>  
+      <div className='md:w-1/3 mt-8'>
+        <div>
+          <h3 className='text-4xl ml-4 bg-blue-400 p-3 rounded-xl'>Spent time on read : {readingTime}</h3>
         </div>
+      <div className="bg-gray-300 ml-4 rounded-xl">
+      <h2 className="text-3xl mt-2 p-2">Bookmarked Blogs: {bookmarks.length}</h2>
+      {
+        bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
+      }  
+    </div>
+    </div>
     );
 };
+
+Bookmarks.propTypes = {
+  bookmarks: PropTypes.array.isRequired,
+  readingTime: PropTypes.number.isRequired
+}
 
 export default Bookmarks;
